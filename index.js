ae787ded4209bf672bec641fd22353b081bf0fa7
Khalid1G <div.khalid1G@gmail.com>
2023-05-20 14:51:40 +0100

    update

--- index.js
+++ index.js
@@ -13,22 +13,53 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Check if it's nighttime (e.g., between 10 PM and 6 AM)
+function isNighttime() {
+  const currentHour = new Date().getHours();
+  return currentHour >= 22 || currentHour < 6;
+}
+
 // Create MQTT client
 const client = mqtt.connect(brokerUrl, options);
 
 // MQTT client connected event
 client.on("connect", () => {
   console.log("Connected to MQTT broker");
+
+  // // Add device with topic "SITI/khalid/test"
+  // const deviceTopic = "SITI/khalid/test2";
+  // client.subscribe(deviceTopic, (err) => {
+  //   if (err) {
+  //     console.error("Error subscribing to device topic:", err);
+  //   } else {
+  //     console.log("Subscribed to device topic:", deviceTopic);
+  //   }
+  // });
 });
 
 // MQTT message received event
-client.on("message", (topic, message) => {
-  console.log("Received message:", message.toString());
-});
+// client.on("message", (topic, message) => {
+//   console.log("Received message:", message.toString());
+// });
 
 // Send random number every minute
 setInterval(() => {
-  const randomNumber = getRandomNumber(0, 85);
+  let randomNumber;
+  if (isNighttime()) {
+    randomNumber = 0;
+  } else {
+    const probability = Math.random();
+    if (probability < 0.1) {
+      randomNumber = 0; // 10% probability for 0
+    } else if (probability < 0.3) {
+      randomNumber = 80; // 20% probability for 80
+    } else if (probability < 0.4) {
+      randomNumber = getRandomNumber(15, 80); // 10% probability for values between 15 and 80
+    } else {
+      randomNumber = getRandomNumber(80, 85); // Remaining 60% probability for values between 80 and 85
+    }
+  }
+
   const message = randomNumber.toString();
   const deviceTopic = "SITI/khalid/test";
   client.publish(deviceTopic, message, (err) => {
@@ -38,4 +69,4 @@ setInterval(() => {
       console.log("Published message:", message);
     }
   });
-}, 60000);
+}, 1000); // Send message every minute
